e8a2941142698cb93852734c20c278d76d8a83b0
quannguyen2153 <quannguyen2153@gmail.com>
2025-04-09 21:34:08 +0700

    update: auto refresh token

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
 import Google from "next-auth/providers/google";
 
-declare module 'next-auth' {
+declare module "next-auth" {
   interface Session {
     accessToken?: string;
     refreshToken?: string;
@@ -9,11 +10,44 @@ declare module 'next-auth' {
   }
 }
 
+async function refreshAccessToken(token: JWT): Promise<JWT> {
+  try {
+    const url =
+      "https://oauth2.googleapis.com/token?" +
+      new URLSearchParams({
+        client_id: process.env.AUTH_GOOGLE_ID!,
+        client_secret: process.env.AUTH_GOOGLE_SECRET!,
+        grant_type: "refresh_token",
+        refresh_token: token.refreshToken as string,
+      });
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+
+    const refreshedTokens = await res.json();
+
+    if (!res.ok) throw refreshedTokens;
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      expiresAt: Math.floor(Date.now() / 1000) + refreshedTokens.expires_in,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.error("Error refreshing access token", error);
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
           scope:
@@ -26,14 +60,24 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   callbacks: {
     async jwt({ token, account }) {
+      const now = Math.floor(Date.now() / 1000);
+
       if (account) {
-        token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-        token.expiresAt = account.expires_at;
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          expiresAt: account.expires_at,
+        };
       }
-      return token;
+
+      if (token.expiresAt && now < (token.expiresAt as number)) {
+        return token;
+      }
+
+      return await refreshAccessToken(token);
     },
-  
+
     async session({ session, token }) {
       session.accessToken = token.accessToken as string;
       session.refreshToken = token.refreshToken as string;
