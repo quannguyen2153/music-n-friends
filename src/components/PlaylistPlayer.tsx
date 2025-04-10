fe25b67269c6dc1e340eb3f702c810cb15a8d9e1
quannguyen2153 <quannguyen2153@gmail.com>
2025-04-11 00:27:13 +0700

    update: enable autoplay

--- src/components/PlaylistPlayer.tsx
+++ src/components/PlaylistPlayer.tsx
@@ -2,7 +2,7 @@
 
 import Playlist from "@/components/Playlist";
 import { Box, Grid } from "@mui/material";
-import YouTube, { YouTubeEvent } from "react-youtube";
+import YouTube, { YouTubeEvent, YouTubePlayer } from "react-youtube";
 import React, { useEffect, useRef, useState } from "react";
 import { PlaylistItem } from "@/types/PlaylistItem";
 
@@ -16,7 +16,8 @@ export default function PlaylistPlayer({
   playlistId,
 }: PlaylistPlayerProps) {
   const [playlist, setPlaylist] = useState<PlaylistItem[]>([]);
-  const [playingVideoIndex, setPlayingVideoIndex] = useState(0);
+  const [playingVideoIndex, setPlayingVideoIndex] = useState(-1);
+  const playerRef = useRef<YouTubePlayer | null>(null);
   const totalResults = useRef(Number.MAX_SAFE_INTEGER);
   const autoFetch = useRef(true);
   const loadingRef = useRef(false);
@@ -58,14 +59,25 @@ export default function PlaylistPlayer({
   };
 
   useEffect(() => {
-    fetchPlaylist();
+    (async () => {
+      await fetchPlaylist();
+      setPlayingVideoIndex(0);
+    })();
   }, []);
 
-  const onVideoEnd = () => {
-    if (playingVideoIndex < playlist.length - 1) {
-      setPlayingVideoIndex((currentVideoIndex) => currentVideoIndex + 1);
+  useEffect(() => {
+    if (playlist.length > 0 && playingVideoIndex < 0) {
+      setPlayingVideoIndex(0);
     }
-  };
+  }, [playlist]);
+
+  useEffect(() => {
+    if (playerRef.current) {
+      playerRef.current.loadVideoById(
+        playlist[playingVideoIndex].contentDetails.videoId
+      );
+    }
+  }, [playingVideoIndex]);
 
   return (
     <Grid container>
@@ -80,14 +92,26 @@ export default function PlaylistPlayer({
       <Grid size={8}>
         <Box width="100%" height="50%">
           <YouTube
-            videoId={playlist[playingVideoIndex]?.contentDetails.videoId}
             opts={{
               height: "100%",
               width: "100%",
               playerVars: { autoplay: 1 },
             }}
-            onReady={(event: YouTubeEvent) => event.target.playVideo()}
-            onEnd={onVideoEnd}
+            onReady={(event: YouTubeEvent) => {
+              playerRef.current = event.target;
+              if (playlist.length > 0) {
+                playerRef.current.loadVideoById(
+                  playlist[playingVideoIndex].contentDetails.videoId
+                );
+              }
+            }}
+            onEnd={() => {
+              if (playingVideoIndex < playlist.length - 1) {
+                setPlayingVideoIndex(
+                  (currentVideoIndex) => currentVideoIndex + 1
+                );
+              }
+            }}
             style={{ height: "100%", width: "100%" }}
           />
         </Box>
